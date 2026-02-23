e3a7dffde84815ebc2e334009641a939ce335644
Adriano Scotti <adriano.scotti10@gmail.com>
2026-02-22 22:18:56 -0300

    testando de novo

--- src/exerciciosOutliers/jogoStarWars/test/NaveEspacialTest01.java
+++ src/exerciciosOutliers/jogoStarWars/test/NaveEspacialTest01.java
@@ -1,7 +1,7 @@
 package exerciciosOutliers.jogoStarWars.test;
 
 import exerciciosOutliers.jogoStarWars.domain.NaveEspacial;
-// testando o git push
+// testando o git push de novo
 public class NaveEspacialTest01 {
     public static void main(String[] args) {
         NaveEspacial nave01 = new NaveEspacial();
